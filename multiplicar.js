592b41d4c03117676f6c7729ab4d7673bd0728e9
Flávio Gomes da Silva Lisboa <flavio.lisboa@fgsl.eti.br>
2025-10-27 10:51:41 -0300

    Cria implementação de multiplicação

--- multiplicar.js
+++ multiplicar.js
@@ -1,18 +1,16 @@
-const prompt = require("prompt-sync")();
+export function multiplicar(numero1,numero2)
+{
+    let contador1 = 0;
+    let contador2 = 0;
+    let produto = 0;
 
-var numero1 = prompt('Número 1: ');
-var numero2 = prompt('Número 2: ');
 
-var contador1 = 0;
-var contador2 = 0;
-var produto = 0;
-
-while (contador1 < numero1) {
-    contador2 = 0;
-    while (contador2 < numero2) {
-        produto++; // $produto = $produto + 1;
-        contador2++;
+    while (contador1 < numero1) {
+        contador2 = 0;
+        while (contador2 < numero2) {
+            produto++; // $produto = $produto + 1;
+            contador2++;
+        }
+        contador1++;
     }
-    contador1++;
 }
-console.log("\nO produto é igual a " + produto + "\n");    
---
+++ multiplicar_test.js
@@ -0,0 +1,11 @@
+import { multiplicar } from './multiplicar.js';
+
+describe('multiplicaçãp', () => {
+  test('multiplica 2 X 3 = 6', () => {
+    expect(sum(2, 3)).toBe(6);
+  });
+
+  test('multiplica 4 X 5 = 20', () => {
+    expect(sum(4, 5)).toBe(20);
+  });
+});
